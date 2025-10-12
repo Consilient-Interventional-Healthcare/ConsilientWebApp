7825976de4af5930092f8c4576bd8dc30588c7e4
Matt Morris <matthewmorris@consilientivh.com>
2025-10-11 20:18:05 -0700

    populate home index data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 using System.Diagnostics;
+using ConsilientWebApp.Data;
 using ConsilientWebApp.Models;
+using ConsilientWebApp.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using static ConsilientWebApp.ViewModels.HomeIndexViewModel;
 
 namespace ConsilientWebApp.Controllers
 {
@@ -9,15 +12,43 @@ namespace ConsilientWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ConsilientContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                            ConsilientContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(TimeframeOptions? timeframe)
         {
-            return View();
+            HomeIndexViewModel viewModel = new HomeIndexViewModel();
+
+            if (timeframe.HasValue)
+                viewModel.SelectedTimeframe = timeframe.Value;
+
+            var userEmail = User?.Identity?.Name;
+            if (!string.IsNullOrEmpty(userEmail))
+            {
+                var employee = _context.Employees.FirstOrDefault(e => e.Email.ToLower() == userEmail.ToLower());
+                if (employee != null)
+                {
+                    var employeeVisits = _context.PatientVisitsStagings.Where(e => e.DateServiced >= viewModel.LowerDateRange).ToList(); // lower date range set within the view model
+                    employeeVisits = employeeVisits.Where(e => e.PhysicianEmployeeId == employee.EmployeeId || e.NursePractitionerEmployeeId == employee.EmployeeId).ToList();
+                    viewModel.EncountersToday = employeeVisits.Count;
+
+                    viewModel.PendingApprovals = employeeVisits.Count(e => e.AddedToMainTable == false);
+                    viewModel.ApprovedEncounters = employeeVisits.Count(e => e.AddedToMainTable == true);
+                    viewModel.ErrorEncounters = 0;
+
+                    var last7Days = DateTime.Today.AddDays(-7);
+                    viewModel.Last7DaysApproved = _context.PatientVisitsStagings.Count(e => e.DateServiced >= DateOnly.FromDateTime(last7Days) && e.AddedToMainTable == true && (e.PhysicianEmployeeId == employee.EmployeeId || e.NursePractitionerEmployeeId == employee.EmployeeId));
+                    viewModel.Last7DaysPending = _context.PatientVisitsStagings.Count(e => e.DateServiced >= DateOnly.FromDateTime(last7Days) && e.AddedToMainTable == false && (e.PhysicianEmployeeId == employee.EmployeeId || e.NursePractitionerEmployeeId == employee.EmployeeId));
+                }
+            }
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
