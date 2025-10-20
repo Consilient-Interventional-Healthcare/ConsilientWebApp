3e2b7f159686fb33788637ac80a91fe5c60b5ec2
Hernan Marano <herchugm@gmail.com>
2025-10-20 15:47:48 -0300

    add api shell

---
+++ Consilient.Api/Consilient.Api/Init/SwaggerBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Consilient.Api.Init
+{
+    internal static class SwaggerBuilderExtensions
+    {
+        public static void AddSwaggerGen(this IServiceCollection services, string appId, string apiVersion)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(apiVersion, new OpenApiInfo
+                {
+                    Title = appId,
+                    Version = apiVersion
+                });
+            });
+        }
+
+        public static IApplicationBuilder UseSwagger(this IApplicationBuilder app, string appId, string apiVersion)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/{apiVersion}/swagger.json", $"{appId} {apiVersion}");
+                c.DocumentTitle = appId;
+                c.RoutePrefix = string.Empty;
+            });
+            return app;
+        }
+    }
+}
